0272f1f59c64793091d1fd91b21f0a3f1ea68dbe
Iván Mervich <imervich@gmail.com>
2014-07-26 19:36:43 -0500

    Added changes for stretch 1.

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -20,9 +20,13 @@
 @property (weak, nonatomic) IBOutlet UILabel *myLabelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
-@property int numberOfTurns;
-@property UILabel *lastTappedLabel;
 @property NSString *currentPlayer;
+
+// stretch 1
+@property (weak, nonatomic) IBOutlet UILabel *draggableLabel;
+@property CGPoint draggableLabelOriginalPosition;
+@property BOOL isDraggingLabel;
+
 
 @end
 
@@ -32,7 +36,7 @@
 {
     [super viewDidLoad];
 
-
+    self.draggableLabelOriginalPosition = self.draggableLabel.center;
     [self initGame];
 }
 
@@ -40,8 +44,8 @@
 {
     self.currentPlayer = @"X";
     self.whichPlayerLabel.text = self.currentPlayer;
-
-    self.numberOfTurns = 0;
+    self.draggableLabel.text = self.currentPlayer;
+    self.draggableLabel.textColor = [UIColor blueColor];
 }
 
 -(UILabel *)findLabelUsingPoint:(CGPoint)point
@@ -52,7 +56,8 @@
     UIView *touchedView = [self.view hitTest:point withEvent:nil];
 
     // if the view is a label, return it
-    if ([touchedView isKindOfClass:[UILabel class]]) {
+    if ([touchedView isKindOfClass:[UILabel class]])
+    {
         touchedLabel = (UILabel *)touchedView;
     }
 
@@ -61,54 +66,44 @@
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGestureRecognizer
 {
-    if (tapGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+    if (tapGestureRecognizer.state == UIGestureRecognizerStateEnded)
+    {
         CGPoint point = [tapGestureRecognizer locationInView:self.view];
-        self.lastTappedLabel = [self findLabelUsingPoint:point];
-
-        // If a label was tapped and it was not already tapped
-        if (self.lastTappedLabel && [self.lastTappedLabel.text length] == 0) {
-
-            // set it's text and text color properties
-            if ([self.currentPlayer isEqualToString:@"X"])
-            {
-                self.lastTappedLabel.text = @"X";
-                self.lastTappedLabel.textColor = [UIColor blueColor];
-            }
-            else
-            {
-                self.lastTappedLabel.text = @"O";
-                self.lastTappedLabel.textColor = [UIColor redColor];
-            }
-
-            // increment the number of labels tapped
-            self.numberOfTurns ++;
-
-            // at least five turns to check who won
-//             if (self.numberOfTurns >= 5)
-//             {
-                 NSString *winner = [self whoWon];
-
-                 if (winner != nil)
-                 {
-                     UIAlertView *alertView = [[UIAlertView alloc] init];
-                     alertView.title = @"Congratulations!";
-                     alertView.message = [NSString stringWithFormat:@"%@ wins!", winner];
-                     alertView.delegate = self;
-                     
-                     [alertView addButtonWithTitle:@"Play again"];
-                     [alertView show];
-
-                     return;
-                 }
-//             }
-
-            // change to next player and show it on whichPlayerLabel
-            [self changeCurrentPlayer];
-            self.whichPlayerLabel.text = self.currentPlayer;
-
-        }
+        UILabel *tappedLabel = [self findLabelUsingPoint:point];
+        [self tapOnLabel:tappedLabel];
     }
 
+}
+
+- (void)tapOnLabel:(UILabel *)label
+{
+    // If label was tapped and it was not already tapped
+    if (label && [label.text length] == 0)
+    {
+        // set it's text and text color properties
+        [self adjustLabelTextAndColorToCurrentPlayer: label];
+
+        NSString *winner = [self whoWon];
+
+        if (winner)
+        {
+            UIAlertView *alertView = [[UIAlertView alloc] init];
+            alertView.title = @"Congratulations!";
+            alertView.message = [NSString stringWithFormat:@"%@ wins!", winner];
+            alertView.delegate = self;
+
+            [alertView addButtonWithTitle:@"Play again"];
+            [alertView show];
+
+            return;
+        }
+
+        // change to next player and show it on whichPlayerLabel
+        [self changeCurrentPlayer];
+        [self adjustLabelTextAndColorToCurrentPlayer:self.draggableLabel];
+        self.whichPlayerLabel.text = self.currentPlayer;
+        
+    }
 }
 
 - (NSString *)whoWon
@@ -164,7 +159,7 @@
 - (BOOL)compareLabelText:(UILabel *)firstLabel secondLabel:(UILabel *)secondLabel thirdLabel:(UILabel *)thirdLabel
 {
     // check for empty labels
-    if ((firstLabel.text == nil || [firstLabel.text isEqualToString:@""]) || (secondLabel.text == nil || [secondLabel.text isEqualToString:@""]) || (thirdLabel.text == nil || [thirdLabel.text isEqualToString:@""]))
+    if ([firstLabel.text length] == 0 || [secondLabel.text length] == 0 || [thirdLabel.text length] == 0)
     {
         return NO;
     }
@@ -179,9 +174,23 @@
     {
         self.currentPlayer = @"O";
     }
-    else if ([self.currentPlayer isEqualToString:@"O"])
+    else
     {
         self.currentPlayer = @"X";
+    }
+}
+
+- (void)adjustLabelTextAndColorToCurrentPlayer:(UILabel *)label
+{
+    if ([self.currentPlayer isEqualToString:@"X"])
+    {
+        label.text = @"X";
+        label.textColor = [UIColor blueColor];
+    }
+    else
+    {
+        label.text = @"O";
+        label.textColor = [UIColor redColor];
     }
 }
 
@@ -199,6 +208,35 @@
     self.myLabelNine.text = nil;
 
     [self initGame];
+}
+
+- (IBAction)onDrag:(UIPanGestureRecognizer *)gestureRecognizer
+{
+    if (gestureRecognizer.state == UIGestureRecognizerStateBegan)
+    {
+        // start drag
+        if (CGRectContainsPoint(self.draggableLabel.frame, [gestureRecognizer locationOfTouch:0 inView:self.view]))
+        {
+            self.isDraggingLabel = YES;
+        }
+    }
+    else if (gestureRecognizer.state == UIGestureRecognizerStateChanged)
+    {
+        // move the label
+        if (self.isDraggingLabel)
+        {
+            self.draggableLabel.center = [gestureRecognizer locationOfTouch:0 inView:self.view];
+        }
+    }
+    else if (gestureRecognizer.state == UIGestureRecognizerStateEnded)
+    {
+        UILabel *targetLabel = [self findLabelUsingPoint:self.draggableLabel.center];
+        [self tapOnLabel:targetLabel];
+
+        // return draggable label it to its original position
+        self.draggableLabel.center = self.draggableLabelOriginalPosition;
+        self.isDraggingLabel = NO;
+    }
 }
 
 @end
